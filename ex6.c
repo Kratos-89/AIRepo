6fc1a062cd249cd0941e7d7a0644d0b2cf5ec4b9
Kratos-89 <zaxkholic@gmail.com>
2025-05-19 23:21:53 +0530

    Did some formatting

--- ex6.c
+++ ex6.c
@@ -3,49 +3,60 @@
 #define MAX 10
 int graph[MAX][MAX], visited[MAX];
 char nodes[MAX];
-void bfs(int start, int n) {
-int queue[MAX], front = 0, rear = 0;
-visited[start] = 1;
-queue[rear++] = start;
-while (front < rear) {
-int current = queue[front++];
-printf("%c ", nodes[current]);
-for (int i = 0; i < n; i++) {
-if (graph[current][i] == 1 && !visited[i]) {
-visited[i] = 1;
-queue[rear++] = i;
+void bfs(int start, int n)
+{
+  int queue[MAX], front = 0, rear = 0;
+  visited[start] = 1;
+  queue[rear++] = start;
+  while (front < rear)
+  {
+    int current = queue[front++];
+    printf("%c ", nodes[current]);
+    for (int i = 0; i < n; i++)
+    {
+      if (graph[current][i] == 1 && !visited[i])
+      {
+        visited[i] = 1;
+        queue[rear++] = i;
+      }
+    }
+  }
 }
-}
-}
-}
-int main() {
-int n, i, j;
-printf("Enter number of nodes: ");
-scanf("%d", &n);
-printf("Enter labels of nodes (e.g. A B C ...):\n");
-for (i = 0; i < n; i++) {
-scanf(" %c", &nodes[i]);
-}
-printf("Enter adjacency matrix (%d x %d):\n", n, n);
-for (i = 0; i < n; i++)
-for (j = 0; j < n; j++)
-scanf("%d", &graph[i][j]);
-// Initialize visited array
-for (i = 0; i < n; i++)
-visited[i] = 0;
-char startNode;
-printf("Enter starting node: ");
-scanf(" %c", &startNode);
-int startIndex = -1;
-for (i = 0; i < n; i++) {
-if (nodes[i] == startNode) {
-startIndex = i;
-break; } }
-if (startIndex == -1) {
-printf("Invalid start node!\n");
-return 1;
-}
-printf("BFS traversal starting from node %c:\n", startNode);
-bfs(startIndex, n);
-return 0;
+int main()
+{
+  int n, i, j;
+  printf("Enter number of nodes: ");
+  scanf("%d", &n);
+  printf("Enter labels of nodes (e.g. A B C ...):\n");
+  for (i = 0; i < n; i++)
+  {
+    scanf(" %c", &nodes[i]);
+  }
+  printf("Enter adjacency matrix (%d x %d):\n", n, n);
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      scanf("%d", &graph[i][j]);
+  // Initialize visited array
+  for (i = 0; i < n; i++)
+    visited[i] = 0;
+  char startNode;
+  printf("Enter starting node: ");
+  scanf(" %c", &startNode);
+  int startIndex = -1;
+  for (i = 0; i < n; i++)
+  {
+    if (nodes[i] == startNode)
+    {
+      startIndex = i;
+      break;
+    }
+  }
+  if (startIndex == -1)
+  {
+    printf("Invalid start node!\n");
+    return 1;
+  }
+  printf("BFS traversal starting from node %c:\n", startNode);
+  bfs(startIndex, n);
+  return 0;
 }
